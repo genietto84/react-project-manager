ce61297e6242ed24c6d4d8f527a04478c47ffb04
moli15 <erikamolino@hotmail.it>
2019-03-13 22:03:01 +0100

    Lifting State Up

--- src/components/Projects/ProjectsGrid/index.js
+++ src/components/Projects/ProjectsGrid/index.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import { ProjectCard } from '../../_common';
 import './index.css';
-import projects from './projects';
 
-class ProjectsGrid extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { projects };
-    }
-    handleClick = project => () => {
-        const projects = this.state.projects.map(item => {
-            if (item.id === project.id)
-                return {
-                    ...item,
-                    done: !item.done
-                }
-            else return item;
-        });
-        this.setState({ projects});
-    }
-    render() {
-        return (
-            <div className='ProjectsGrid'>
-                {this.state.projects.map(item => (
-                    <ProjectCard
-                        item = {item}
-                        key = {item.id}
-                        handleClick = {this.handleClick(item)}
-                    />
-                ))}
-            </div>
-        )
-    }
-}
+const ProjectsGrid = props => (
+    <div className='ProjectsGrid'>
+        {props.projects.map(item => (
+            <ProjectCard
+                item = {item}
+                key = {item.id}
+                handleClick = {props.handleClick(item)}
+            />
+        ))}
+    </div>
+)
 
 export default ProjectsGrid;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ProjectsHeader from './ProjectsHeader';
 import ProjectsGrid from './ProjectsGrid';
+import projects from './projects';
 
-const Projects = props => (
-    <div className="Projects">
-        <ProjectsHeader title='I miei progetti' buttonText='Aggiungi progetto' />
-
-        <ProjectsGrid />        
-    </div>
-);
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { projects };
+    }
+    handleClick = project => () => {
+        const projects = this.state.projects.map(item => {
+            if (item.id === project.id)
+                return {
+                    ...item,
+                    done: !item.done
+                }
+            else return item;
+        });
+        this.setState({ projects});
+    }
+    render() {
+        return (
+            <div className="Projects">
+                <ProjectsHeader title='I miei progetti' buttonText='Aggiungi progetto' />
+        
+                <ProjectsGrid
+                    projects = {this.state.projects}
+                    handleClick = {this.handleClick}
+                />
+            </div>
+        )
+    }
+}
 
 export default Projects;
