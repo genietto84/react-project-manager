6f9826edf0f420e28db618aeeb27e45456714ea0
moli15 <erikamolino@hotmail.it>
2019-03-13 18:32:55 +0100

    map projects array

--- src/components/Projects/ProjectsGrid/index.js
+++ src/components/Projects/ProjectsGrid/index.js
@@ -1,48 +1,16 @@
 import React from 'react';
 import { ProjectCard } from '../../_common';
 import './index.css';
+import projects from './projects';
 
 const ProjectsGrid = props => (
     <div className='ProjectsGrid'>
-        <ProjectCard
-            item={{
-                name: 'Progetto Alpha',
-                start: '2019-03-31',
-                end: '2019-06-15',
-                description: 'Lorem ipsum dolor sit amet.',
-                priority: 'medium',
-                done: true,
-            }}
-        />
-        <ProjectCard
-            item={{
-                name: 'Progetto Beta',
-                start: '2019-03-31',
-                end: '2019-06-15',
-                description: 'Lorem ipsum dolor sit amet.',
-                priority: 'low',
-                done: true,
-            }}
-        />
-        <ProjectCard
-            item={{
-                name: 'Progetto Gamma',
-                start: '2019-03-31',
-                end: '2019-06-15',
-                description: 'Lorem ipsum dolor sit amet.',
-                priority: 'low',
-                done: true,
-            }}
-        />
-        <ProjectCard
-            item={{
-                name: 'Progetto Delta',
-                start: '2019-03-31',
-                description: 'Lorem ipsum dolor sit amet.',
-                priority: 'low',
-                done: false,
-            }}
-        />
+        {projects.map(item => (
+            <ProjectCard
+                item = {item}
+                key = {item.id}
+            />
+        ))}
     </div>
 );
 
---
+++ src/components/Projects/ProjectsGrid/projects.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 'NHusYJl',
+        name: 'Progetto Alpha',
+        start: '2019-03-31',
+        end: '2019-06-15',
+        description: 'Lorem ipsum dolor sit amet.',
+        priority: 'medium',
+        done: true,
+    }, {
+        id: 'SJieYKl',
+        name: 'Progetto Beta',
+        start: '2019-03-31',
+        end: '2019-06-15',
+        description: 'Lorem ipsum dolor sit amet.',
+        priority: 'low',
+        done: true,
+    }, {
+        id: 'a5DefFF',
+        name: 'Progetto Gamma',
+        start: '2019-03-31',
+        end: '2019-06-15',
+        description: 'Lorem ipsum dolor sit amet.',
+        priority: 'low',
+        done: true,
+    }, {
+        id: 'POjeGBs',
+        name: 'Progetto Delta',
+        start: '2019-03-31',
+        description: 'Lorem ipsum dolor sit amet.',
+        priority: 'low',
+        done: false,
+    }    
+]
