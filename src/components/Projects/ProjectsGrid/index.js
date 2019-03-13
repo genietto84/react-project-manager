4a7f8bbfd8a82563659965f9b83634c9b0708a07
moli15 <erikamolino@hotmail.it>
2019-03-13 16:21:55 +0100

    ProjectsGrid component

---
+++ src/components/Projects/ProjectsGrid/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProjectsGrid = props => (
+    <div className='ProjectsGrid'>
+        Grid
+    </div>
+);
+
+export default ProjectsGrid;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ProjectsHeader from './ProjectsHeader';
+import ProjectsGrid from './ProjectsGrid';
 
 const Projects = props => (
     <div className="Projects">
         <ProjectsHeader title='I miei progetti' buttonText='Aggiungi progetto' />
+
+        <ProjectsGrid />        
     </div>
 );
 
