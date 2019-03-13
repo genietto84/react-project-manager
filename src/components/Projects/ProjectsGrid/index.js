2eb3216b890beebda631d0ae6d9be664e1efd6d7
moli15 <erikamolino@hotmail.it>
2019-03-13 16:29:42 +0100

    Card semantic in ProjectsGrid

--- src/components/Projects/ProjectsGrid/index.js
+++ src/components/Projects/ProjectsGrid/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Card, Button } from 'semantic-ui-react';
 
 const ProjectsGrid = props => (
     <div className='ProjectsGrid'>
-        Grid
+        <Card>
+            <Card.Content>
+                <Card.Header>
+                    Progetto Alpha
+                </Card.Header>
+                <Card.Meta>
+                    2019-10-10 | 2019-10-11
+                </Card.Meta>
+                <Card.Description>
+                    dksksadjdsajk kljdsakljsd akljd lkas
+                </Card.Description>
+            </Card.Content>
+            <Card.Content>
+                <Button fluid>
+                    Fai Qualcosa
+                </Button>
+            </Card.Content>
+        </Card>
     </div>
 );
 
