101637b411327e4828cf5241e0fb49a7be8e8b72
moli15 <erikamolino@hotmail.it>
2019-03-14 13:57:47 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Header }  from './components/_common';
 import Projects from './components/Projects';
 import './App.css';
@@ -6,15 +7,17 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Header title='project manager' nav='projects' />
-                
-                <div className="content">
-                    <div className="wrapper">
-                        <Projects />
+            <Router>
+                <div className="App">
+                    <Header title='project manager' nav='projects' />
+                    
+                    <div className="content">
+                        <div className="wrapper">
+                            <Projects />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
