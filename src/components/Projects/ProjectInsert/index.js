4b962c0e95ea2c56db68ec4821672b79a97e4638
moli15 <erikamolino@hotmail.it>
2019-03-14 17:39:02 +0100

    select in Form e style

--- src/components/Projects/ProjectInsert/index.js
+++ src/components/Projects/ProjectInsert/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Form } from 'semantic-ui-react';
+import './index.css';
 
 class ProjectInsert extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
+            name: '',
+            priority: ''
         }
     }
     handleChange = event => {
@@ -16,26 +18,51 @@ class ProjectInsert extends Component {
     }
     handleSubmit = event => {
         event.preventDefault();
-        console.log(this.state)
+        console.log(this.state);
     }
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name
-                        <input
-                            type="text"
-                            name='name'
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                            required
-                        />
-                    </label>
-                    <Button type="submit" color='green'>
-                        Crea
-                    </Button>
-                </form>
+                <div className='ProjectForm'>
+                    <Form onSubmit={this.handleSubmit}>
+                        <div className='row'>
+                            <div className="field">
+                                <label>
+                                    Name
+                                    <input
+                                        type="text"
+                                        name='name'
+                                        value={this.state.name}
+                                        onChange={this.handleChange}
+                                        required
+                                    />
+                                </label>
+                            </div>
+                            <div className="field">
+                                <label>
+                                    Priority:
+                                    <select
+                                        name='priority'
+                                        value={this.state.priority}
+                                        onChange={this.handleChange}
+                                        required
+                                    >
+                                        <option value='' disabled>Select priority</option>
+                                        <option value="low">Low</option>
+                                        <option value="medium">Medium</option>
+                                        <option value="hard">Hard</option>
+                                    </select>    
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="buttonContainer">
+                            <Button type="submit" color='green'>
+                                Crea
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
             </div>
         );
     }
