9e3d1f94998f518baa236448badcd8dda9029827
moli15 <erikamolino@hotmail.it>
2019-03-14 17:17:47 +0100

    initial form in ProjectInsert

--- src/components/Projects/ProjectInsert/index.js
+++ src/components/Projects/ProjectInsert/index.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class ProjectInsert extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        }
+    }
+    handleChange = event => {
+        if (event) {
+            const { name, value } = event.target;
+            this.setState({[name]: value});
+        }
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state)
+    }
     render() {
         return (
             <div>
-                ProjectInsert
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name
+                        <input
+                            type="text"
+                            name='name'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </label>
+                    <Button type="submit" color='green'>
+                        Crea
+                    </Button>
+                </form>
             </div>
         );
     }
